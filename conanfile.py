fdda1c1b5b55b14bf2ecfb572b04ce194d409903
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:29:16 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -3,20 +3,20 @@ from conans import ConanFile, tools, os
 
 class BoostAtomicConan(ConanFile):
     name = "Boost.Atomic"
-    version = "1.64.0"
+    version = "1.65.1"
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-atomic"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["atomic"]
     default_options = "shared=False"
     options = {"shared": [True, False]}
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
-    requires = "Boost.Config/1.64.0@bincrafters/testing", \
-        "Boost.Assert/1.64.0@bincrafters/testing", \
-        "Boost.Type_Traits/1.64.0@bincrafters/testing"
+    requires = "Boost.Config/1.65.1@bincrafters/testing", \
+        "Boost.Assert/1.65.1@bincrafters/testing", \
+        "Boost.Type_Traits/1.65.1@bincrafters/testing"
  
     def source(self):
         boostorg_github = "https://github.com/boostorg"

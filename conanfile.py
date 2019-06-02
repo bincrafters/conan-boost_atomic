45d438d429b8e1d1006182155bb8b312d1113125
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:24 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,17 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostAtomicConan(base.BoostBaseConan):
     name = "boost_atomic"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_atomic"
-    lib_short_names = ["atomic"]
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
